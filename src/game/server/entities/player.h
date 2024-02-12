4734617b387945078d21329fc8f35198ce6e8de0
Mikk <martimsanchez155@gmail.com>
2024-02-12 07:41:45 -0300

    Define LeaveObserver

--- src/game/server/entities/player.h
+++ src/game/server/entities/player.h
@@ -153,6 +153,7 @@ public:
 	int m_iObserverWeapon;	 // weapon of current tracked target
 	int m_iObserverLastMode; // last used observer mode
 	bool IsObserver() { return 0 != pev->iuser1; }
+	bool m_bObserverSurvival = false;
 
 	int random_seed; // See that is shared between client & server for shared weapons code
 
@@ -333,7 +334,7 @@ public:
 	Vector BodyTarget(const Vector& posSrc) override { return Center() + pev->view_ofs * RANDOM_FLOAT(0.5, 1.1); } // position to shoot at
 	bool IsAlive() override { return (pev->deadflag == DEAD_NO) && pev->health > 0; }
 	bool ShouldFadeOnDeath() override { return false; }
-	bool IsPlayer() override { return true; } // Spectators should return false for this, they aren't "players" as far as game logic is concerned
+	bool IsPlayer() override { return true; } // Spectators should return false for this, they aren't "players" as far as game logic is concerned, No. return true, check if is observer instead. -Mikk
 
 	bool IsBot() const { return (pev->flags & FL_FAKECLIENT) != 0; }
 
@@ -421,6 +422,7 @@ public:
 	 */
 	void StartDeathCam();
 	void StartObserver(Vector vecPosition, Vector vecViewAngle);
+	void LeaveObserver();
 
 	void AddPoints(int score, bool bAllowNegativeScore);
 	void AddPointsToTeam(int score, bool bAllowNegativeScore);
