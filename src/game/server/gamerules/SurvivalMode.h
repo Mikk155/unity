632ece77a399c63ec0a76309ca633ec4518a180a
Mikk <martimsanchez155@gmail.com>
2024-02-12 07:51:57 -0300

    to do list

--- src/game/server/gamerules/SurvivalMode.h
+++ src/game/server/gamerules/SurvivalMode.h
@@ -13,6 +13,39 @@
  *
  ****/
 
+/*
+    -Mikk -TODO
+
+    - Fix players re-spawning gibing other players just as how re-spawning works in sven coop.
+
+    - CBaseEntity::Revive( CBaseEntity* pOther )
+        pOther is the reviver, mean player by medkit or checkpoint entity
+
+    - CBasePlayer::Revive( CBaseEntity* pOther ) override;
+        Meant for this->LeaveObserver, get position of dead corpse and teleport after Spawn
+
+    - CBaseDeadPlayer
+        overrides the usage of regular HL player's corpses, one entity per player.
+        name it "deadplayer" for keep compatibility with sven
+        owner must be the player
+        this entity should be stored in the player for easy access
+
+    - CWeaponMedkit
+        Medkit weapon for reviving entities and players
+        Should consume healthstations/healthkits instead of ilimited >:x
+        item_healthkit will equip this weapon.
+        create a cvar for prevent said feature and use weapon_medkit item instead
+        primary attack healths yourself if not aiming to anyone
+        When taking health, give medkit ammo first if not full
+
+    - CPointCheckPoint
+        directly calls Revive function of all connected players
+        nothing more, leave it as simple as it can be.
+        instead in a future with Angelscript
+        Scripters would have access to CBaseEntity::m_szKeyValues
+        as a dictionary for getting keyvalues and re-mapping their own checkpoints
+*/
+
 #pragma once
 
 #include "cbase.h"
