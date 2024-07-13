afeedf675271ed707e4e2f73295110de4e0b65b1
Mikk155 <martimsanchez155@gmail.com>
2024-07-13 01:38:04 -0300

    Update skill_to_json.py

--- unity/mods/tools/skill_to_json.py
+++ unity/mods/tools/skill_to_json.py
@@ -1,29 +1,56 @@
+# ===============================================================================
+# convert skill.cfg to our json-formatting skills
+# ===============================================================================
+
 import os
-from tools.path import unity
+
+from tools.path import port, unity
+from tools.jsonc import load_json
 
 def skill_to_json( path='' ):
-    filetext = f'{unity}/mods/port/{path}'
-    with open( f'{filetext}', 'r' ) as txt, open( filetext.replace( ".cfg", ".json" ), 'w' ) as json:
-        print(f'[skill_to_json] Converting {path} to JSON' )
-        json.write( '{\n' )
+
+    filetext = f'{port}/{path}'
+
+    print(f'[skill_to_json] Converting {path} to JSON' )
+
+    with open( f'{filetext}', 'r' ) as txt, open( filetext.replace( ".cfg", ".json" ), 'w' ) as jsonfile:
+
+        ogskills = load_json( path=f'{unity}/cfg/skill.json' )
+
+        jsonfile.write( '{\n' )
         FirstLineOf = True
         lines = txt.readlines()
+
         for line in lines:
             line = line.strip()
+
             if not line or line == '' or line.startswith( '//' ):
                 continue
+
             sk = line.split()
             sk[0] = sk[0].strip()
             if len(sk) <= 2:
                 sk.append( '0' )
             sk[1] = sk[1].strip()
             sk[1] = sk[1].strip( '"' )
+
+            if sk[0].startswith( 'sk_' ):
+                sk[0] = sk[0][ 3 : ]
+
+            # Compare with the original skills
+            if sk[0] in ogskills or sk[0][ len(sk[0]) - 1 : ] in [ '1', '2', '3' ] and sk[0][ : len(sk[0]) - 1  ] in ogskills:
+                continue
+
             if not FirstLineOf:
-                json.write( ',\n' )
+                jsonfile.write( ',\n' )
             else:
                 FirstLineOf = False
-            json.write(f'\t"{sk[0]}": {sk[1]}')
-        json.write( '\n}' )
+
+            jsonfile.write(f'\t"{sk[0]}": {sk[1]}')
+
+        jsonfile.write( '\n}\n' )
+
         txt.close()
-        json.close()
+        jsonfile.close()
+
         os.remove( f'{filetext}' )
