3a041b27276feed3f5743ec8f2316bf5475bf519
Mikk155 <martimsanchez155@gmail.com>
2024-07-13 01:37:18 -0300

    Update copy_assets.py

--- unity/mods/tools/copy_assets.py
+++ unity/mods/tools/copy_assets.py
@@ -1,12 +1,24 @@
+# ===============================================================================
+# FCopy assets from the mod folder to unity/mods/port
+# ===============================================================================
+
 import shutil, os
+
 from tools.path import port, halflife
 
 def copy_assets( mod='', assets={} ):
+
+    print(f'Copying asset...')
+
     for In, Out in assets.items():
+
         if Out.find( '/' ) != -1:
             path = f'{port}\{Out}'
             path = path[ : path.rfind( f'/' ) ]
+
             if not os.path.exists( path ):
                 os.makedirs( path )
+
+        print(f'{In}  >  {Out}')
+
         shutil.copy( f'{halflife}\{mod}\{In}', f'{port}\{Out}' )
-        print(f'Copying asset {In} > {Out}')
