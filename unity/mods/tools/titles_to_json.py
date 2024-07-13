d16730672cf2fbbfbfd57bd884be96b34bee8efe
Mikk155 <martimsanchez155@gmail.com>
2024-07-13 01:38:13 -0300

    Update titles_to_json.py

--- unity/mods/tools/titles_to_json.py
+++ unity/mods/tools/titles_to_json.py
@@ -1,60 +1,96 @@
+# ===============================================================================
+# convert titles.txt to our json-formatted titles
+# ===============================================================================
+
 import os
-from tools.path import unity
+
+from tools.path import port, unity
+from tools.jsonc import load_json
 
 def titles_to_json( path='' ):
-    filetext = f'{unity}/mods/port/{path}'
-    with open( f'{filetext}', 'r' ) as txt, open( filetext.replace( ".txt", ".json" ), 'w' ) as json:
-        print(f'[titles_to_json] Converting {path} to JSON' )
-        json.write( '{\n' )
+
+    filetext = f'{port}/{path}'
+
+    print(f'[titles_to_json] Converting {path} to JSON' )
+
+    with open( f'{filetext}', 'r' ) as txt, open( filetext.replace( ".txt", ".json" ), 'w' ) as jsfile:
+
+        ogtitles = load_json( path=f'{unity}/cfg/titles.json' )
+
+        jsfile.write( '{\n' )
         FirstLineOf = True
         lines = txt.readlines()
         Title = ''
         Message = ''
         Params = {}
+
         for line in lines:
             line = line.strip()
+
             if not line or line == '' or line.startswith( '//' ) or line.startswith( '{' ):
                 continue
+
             if line.startswith( '}' ):
+
+                Message = Message.replace( '"', '\\"')
+
+                # Compare with the original titles
+                if Title in ogtitles and Message == ogtitles.get( Title, {} ).get( "english", "" ).replace( '\n', '\\n' ):
+                    Message = ''
+                    Title = ''
+                    continue
+
                 if not FirstLineOf:
-                    json.write( ',\n' )
+                    jsfile.write( ',\n' )
                 else:
                     FirstLineOf = False
-                Message = Message.replace( '"', '\\"')
-                json.write(f'\t"{Title}":\n')
-                json.write('\t{\n')
+
+                jsfile.write(f'\t"{Title}":\n')
+                jsfile.write('\t{\n')
                 FirstSegOf = True
+
                 for p, a in Params.items():
+
                     if not FirstSegOf:
-                        json.write( ',\n' )
+                        jsfile.write( ',\n' )
                     else:
                         FirstSegOf = False
+
                     s = str(a).replace("'", "")
-                    json.write(f'\t\t"{p}": {s}')
+                    jsfile.write(f'\t\t"{p}": {s}')
+
                 if not FirstSegOf:
-                    json.write(f',\n')
-                json.write(f'\t\t"english": "{Message}"\n')
-                json.write('\t}')
+                    jsfile.write(f',\n')
+
+                jsfile.write(f'\t\t"english": "{Message}"\n')
+                jsfile.write('\t}')
                 Message = ''
                 Title = ''
+
             elif line.startswith( '$' ):
                 line = line[ 1: ]
                 parameters = line.split()
+
                 if len(parameters) > 1:
                     if len(parameters) > 2:
                         pairs = parameters.copy()
                         pairs.pop(0)
                         Params[ parameters[0] ] = pairs
+
                     else:
                         try:
                             Params[ parameters[0] ] = float(parameters[1])
                         except Exception as e:
                             Params[ parameters[0] ] = 0
+
             elif not Title or Title == '':
                 Title = line
             else:
                 Message = f'{Message}{line}\\n'
-        json.write( '\n}' )
+
+        jsfile.write( '\n}\n' )
+
         txt.close()
-        json.close()
+        jsfile.close()
+
         os.remove( f'{filetext}' )
