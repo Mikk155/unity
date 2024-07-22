fffc4e4e3bbe8a815b43786ef4771996eef4683e
Mikk155 <martimsanchez155@gmail.com>
2024-07-22 08:13:02 -0300

    Implement trigger_entity_condition

---
+++ src/game/server/entities/trigger_entity_condition.cpp
@@ -0,0 +1,95 @@
+/***
+ *
+ *	Copyright (c) 1996-2001, Valve LLC. All rights reserved.
+ *
+ *	This product contains software technology licensed from Id
+ *	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
+ *	All Rights Reserved.
+ *
+ *   Use, distribution, and modification of this source code and/or resulting
+ *   object code is restricted to non-commercial enhancements to products from
+ *   Valve LLC.  All other use, distribution, or modification is prohibited
+ *   without written permission from Valve LLC.
+ *
+ ****/
+
+#include "cbase.h"
+
+class CTriggerEntityCondition : public CPointEntity
+{
+	DECLARE_CLASS( CTriggerEntityCondition, CPointEntity );
+	DECLARE_DATAMAP();
+
+    public:
+        bool KeyValue( KeyValueData* pkvd ) override;
+        void Use( CBaseEntity* pActivator, CBaseEntity* pCaller, USE_TYPE useType, float value ) override;
+
+    private:
+        string_t m_PassTarget;
+        string_t m_FailTarget;
+        int m_Condition;
+        string_t m_Arg1;
+};
+
+LINK_ENTITY_TO_CLASS( trigger_entity_condition, CTriggerEntityCondition );
+
+BEGIN_DATAMAP( CTriggerEntityCondition )
+    DEFINE_FIELD( m_PassTarget, FIELD_STRING ),
+	DEFINE_FIELD( m_FailTarget, FIELD_STRING ),
+	DEFINE_FIELD( m_Condition, FIELD_INTEGER ),
+	DEFINE_FIELD( m_Arg1, FIELD_STRING ),
+END_DATAMAP();
+
+bool CTriggerEntityCondition :: KeyValue( KeyValueData* pkvd )
+{
+	if( FStrEq( pkvd->szKeyName, "pass_target" ) )
+	{
+		m_PassTarget = ALLOC_STRING( pkvd->szValue );
+	}
+	else if( FStrEq( pkvd->szKeyName, "fail_target" ) )
+	{
+		m_FailTarget = ALLOC_STRING( pkvd->szValue );
+	}
+	else if( FStrEq( pkvd->szKeyName, "condition" ) )
+	{
+		m_Condition = atoi( pkvd->szValue );
+	}
+	else if( FStrEq( pkvd->szKeyName, "argument_1" ) )
+	{
+		m_Arg1 = ALLOC_STRING( pkvd->szValue );
+	}
+    else
+    {
+    	return CPointEntity::KeyValue(pkvd);
+    }
+	return true;
+}
+
+void CTriggerEntityCondition :: Use( CBaseEntity* pActivator, CBaseEntity* pCaller, USE_TYPE useType, float value )
+{
+    bool bCondition = false;
+
+    if( pActivator != nullptr )
+    {
+        switch( m_Condition )
+        {
+            case 0: {
+                bCondition = pActivator->IsPlayer();
+                break;
+            }
+            case 1: {
+                bCondition = pActivator->IsPlayer() && ToBasePlayer( pActivator )->HasSuit();
+                break;
+            }
+            case 2: {
+                if( !FStringNull( m_Arg1 ) )
+                    bCondition = pActivator->IsPlayer() && ToBasePlayer( pActivator )->HasNamedPlayerWeapon( STRING( m_Arg1 ) );
+                else
+                    bCondition = pActivator->IsPlayer() && ToBasePlayer( pActivator )->HasWeapons();
+                break;
+            }
+        }
+    }
+
+    FireTargets( ( bCondition ? STRING( m_PassTarget ) : STRING( m_FailTarget ) ), pActivator, this, USE_TOGGLE, value );
+}
