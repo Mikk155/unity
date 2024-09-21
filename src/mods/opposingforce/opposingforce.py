8879e3fca7f6486b4a3c32f1abfa6b87147531b1
Mikk155 <martimsanchez155@gmail.com>
2024-09-21 18:25:45 -0300

    Add more fixes to op4

--- src/mods/opposingforce/opposingforce.py
+++ src/mods/opposingforce/opposingforce.py
@@ -47,6 +47,40 @@ def PostMapUpgrade( index : int, entity : unity.Entity, map : str ):
         if entity.classname == 'monster_op4loader':
             entity.skin = 1;
 
+    # Fixes <c>monster_generic</c> entities that use <c>hgrunt_opfor.mdl</c> to use the correct body value.
+    elif map == 'of2a2':
+
+        if entity.targetname in [ 'fgrunt1', 'fgrunt2' ]:
+            entity.body = 1;
+
+    # Fixes the Pit Worm's Nest bridge possibly breaking if triggered too soon.
+    elif map == 'of4a4':
+
+        if entity.targetname == 'kill_pitworm_mm':
+            entity.bridge_global_trigger = 30;
+            entity.bridge_mm = 30;
+
+    # Fixes the Bullsquids in <c>of5a2</c> having the wrong targetnames causing the eating scripts to fail.
+    elif map == 'of5a2':
+
+        if entity.targetname == 't27':
+            entity.targetname = 'feeder_1';
+        elif entity.targetname == 't6':
+            entity.targetname = 'feeder_2';
+
+    # Adds a missing <c>.wav</c> extension to the sound played when the Geneworm enters in <c>of6a5</c>.
+    elif map == 'of6a5':
+
+        if entity.targetname == 'genearrivesound':
+            entity.message += '.wav';
+
+    # Prevents the Osprey in <c>ofboot1</c> from switching to the <c>rotor</c> animation and falling through the ground after loading a save game.
+    elif map == 'ofboot1':
+
+        if entity.classname == 'scripted_sequence' and entity.m_iszEntity == 'osprey':
+            # Naming the script prevents it from immediately completing and removing the script itself.
+            entity.targetname = 'osprey_script';
+
     return entity
 
 for mapname, path in maps.items():
