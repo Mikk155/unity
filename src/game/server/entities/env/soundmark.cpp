c43643f0499a3ab56414a6a7d803fccf43e096ca
Mikk155 <martimsanchez155@gmail.com>
2024-09-21 15:49:32 -0300

    Entity env_soundmark

---
+++ src/game/server/entities/env/soundmark.cpp
@@ -0,0 +1,79 @@
+/***
+ *
+ *	Copyright (c) 1996-2001, Valve LLC. All rights reserved.
+*
+*	This product contains software technology licensed from Id
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
+*	All Rights Reserved.
+*
+*   Use, distribution, and modification of this source code and/or resulting
+*   object code is restricted to non-commercial enhancements to products from
+*   Valve LLC.  All other use, distribution, or modification is prohibited
+*   without written permission from Valve LLC.
+*
+****/
+
+#include "cbase.h"
+#include "soundent.h"
+
+class CSoundMark : public CPointEntity
+{
+	DECLARE_CLASS( CSoundMark, CPointEntity );
+	DECLARE_DATAMAP();
+
+	public:
+		bool KeyValue( KeyValueData* pkvd ) override;
+        void Use( CBaseEntity* pActivator, CBaseEntity* pCaller, USE_TYPE useType, float value ) override;
+
+	private:
+		int m_iSoundType;
+        float m_fSoundVolume;
+        float m_fSoundDuration;
+        string_t m_sOriginEntity;
+};
+
+BEGIN_DATAMAP( CSoundMark )
+	DEFINE_FIELD( m_iSoundType, FIELD_INTEGER ),
+	DEFINE_FIELD( m_fSoundVolume, FIELD_FLOAT ),
+	DEFINE_FIELD( m_fSoundDuration, FIELD_FLOAT ),
+	DEFINE_FIELD( m_sOriginEntity, FIELD_STRING ),
+END_DATAMAP();
+
+
+LINK_ENTITY_TO_CLASS( env_soundmark, CSoundMark );
+
+bool CSoundMark :: KeyValue( KeyValueData* pkvd )
+{
+	if( fStrEq( pkvd->szKeyName, "sound_type" ) )
+	{
+        m_iSoundType = atoi( pkvd->szValue );
+	}
+	else if( fStrEq( pkvd->szKeyName, "sound_origin" ) )
+	{
+        m_sOriginEntity = ALLOC_STRING( pkvd->szValue );
+	}
+	else if( fStrEq( pkvd->szKeyName, "sound_volume" ) )
+	{
+        m_fSoundVolume = atof( pkvd->szValue );
+	}
+	else if( fStrEq( pkvd->szKeyName, "sound_duration" ) )
+	{
+        m_fSoundDuration = atof( pkvd->szValue );
+	}
+	else
+	{
+		return BaseClass::KeyValue( pkvd );
+	}
+
+	return true;
+}
+
+void CSoundMark :: Use( CBaseEntity* pActivator, CBaseEntity* pCaller, USE_TYPE useType, float value )
+{
+    if( !UTIL_IsMasterTriggered( m_sMaster, pActivator, m_UseLocked ) )
+        return;
+
+    auto pTarget = AllocNewActivator( pActivator, pCaller, m_sOriginEntity );
+
+    CSoundEnt::InsertSound( m_iSoundType, ( pTarget != nullptr ? pTarget->pev->origin : pev->origin ), m_fSoundVolume, m_fSoundDuration );
+}
